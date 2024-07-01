0bf4f3baf89b953a00adeebb48fd6ef18b4723d1
LukaZanki <lukazanki@gmail.com>
2024-07-01 18:22:10 +0200

    feature-4 remove unused dispatcher

--- app/src/main/java/com/example/testrawg/data/repository/UserDataRepositoryImpl.kt
+++ app/src/main/java/com/example/testrawg/data/repository/UserDataRepositoryImpl.kt
@@ -1,16 +1,12 @@
 package com.example.testrawg.data.repository
 
 import com.example.testrawg.data.db.datastore.PreferencesDataSource
-import com.example.testrawg.di.Dispatcher
-import com.example.testrawg.di.RawgDispatchers
 import com.example.testrawg.domain.repository.UserDataRepository
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserDataRepositoryImpl @Inject constructor(
     private val preferencesDataSource: PreferencesDataSource,
-    @Dispatcher(RawgDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ) : UserDataRepository {
     override val shouldShowOnboarding: Flow<Boolean> =
         preferencesDataSource.shouldShowOnboarding
